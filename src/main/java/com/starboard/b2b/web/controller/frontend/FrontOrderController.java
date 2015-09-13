99c30162ae6e696322840a26e9118e64ce5d8ea5
magicalcyber <magicalcyber@gmail.com>
2015-09-14 00:12:39 +0700

    Prepare code for Order step2

--- src/main/java/com/starboard/b2b/web/controller/frontend/FrontOrderController.java
+++ src/main/java/com/starboard/b2b/web/controller/frontend/FrontOrderController.java
@@ -4,14 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/frontend/order")
 public class FrontOrderController {
 
 	@RequestMapping(value = "index", method = RequestMethod.GET)
-	String index(Model model) {
+	String step1(Model model) {
 		return "pages-front/order/step1_brand";
+	}
+
+	@RequestMapping(value = "step2", method = RequestMethod.GET)
+	String step2(Integer brandId, Model model) {
+		
+		return "pages-front/order/step2_address";
 	}
 
 	@RequestMapping(value = "create", method = RequestMethod.GET)
--- src/main/resources/script/b2b - insert app_config.sql
+++ src/main/resources/script/b2b - insert app_config.sql
@@ -1,2 +1,1 @@
-insert into  app_config (config_key, config_value, created_by, created_date)
-values('upload_path', '/Users/magicalcyber/Pictures/b2b_upload', 'B2B', curdate());
+insert into  app_config (config_key, config_value, created_by, created_date) values('upload_path', '/Users/magicalcyber/Pictures/b2b_upload', 'B2B', curdate());
