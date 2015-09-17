37f3e31af0a6b95f6c33608549cc07146aa97d12
Ken <yutthagarn.ken@gmail.com>
2015-09-18 01:47:58 +0700

    Config auto import data with start server

--- src/main/java/com/starboard/b2b/config/RepositoryConfig.java
+++ src/main/java/com/starboard/b2b/config/RepositoryConfig.java
@@ -1,9 +1,7 @@
 package com.starboard.b2b.config;
 
-import java.util.Properties;
-
-import javax.sql.DataSource;
-
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -15,65 +13,65 @@ import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
 @PropertySource(value = "classpath:application-${spring.profiles.active}.properties")
 public class RepositoryConfig {
 
-	// private static final Logger log =
-	// LoggerFactory.getLogger(RepositoryConfig.class);
+    // private static final Logger log =
+    // LoggerFactory.getLogger(RepositoryConfig.class);
 
-	@Autowired
-	private Environment env;
+    @Autowired
+    private Environment env;
 
-	@Bean
-	public HibernateExceptionTranslator hibernateExceptionTranslator() {
-		return new HibernateExceptionTranslator();
-	}
+    @Bean
+    public HibernateExceptionTranslator hibernateExceptionTranslator() {
+        return new HibernateExceptionTranslator();
+    }
 
-	@Bean
-	public LocalSessionFactoryBean sessionFactory() {
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-		sessionFactory.setDataSource(dataSource());
-		sessionFactory.setPackagesToScan("com.starboard.b2b.model");
-		sessionFactory.setHibernateProperties(hibernateProperties());
-		return sessionFactory;
-	}
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.starboard.b2b.model");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
 
-	@Bean
-	public DataSource dataSource() {
-		HikariConfig config = new HikariConfig();
-		config.setDriverClassName(env.getProperty("jdbc.driver.class_name"));
-		config.setJdbcUrl(env.getProperty("hibernate.hikari.dataSource.url"));
-		config.setUsername(env.getProperty("hibernate.hikari.dataSource.user"));
-		config.setPassword(env.getProperty("hibernate.hikari.dataSource.password"));
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(env.getProperty("jdbc.driver.class_name"));
+        config.setJdbcUrl(env.getProperty("hibernate.hikari.dataSource.url"));
+        config.setUsername(env.getProperty("hibernate.hikari.dataSource.user"));
+        config.setPassword(env.getProperty("hibernate.hikari.dataSource.password"));
 
-		config.addDataSourceProperty("cachePrepStmts", env.getProperty("hibernate.hikari.dataSource.cachePrepStmts"));
-		config.addDataSourceProperty("prepStmtCacheSize",
-				env.getProperty("hibernate.hikari.dataSource.prepStmtCacheSize"));
-		config.addDataSourceProperty("prepStmtCacheSqlLimit",
-				env.getProperty("hibernate.hikari.dataSource.prepStmtCacheSqlLimit"));
-		config.addDataSourceProperty("providerClass", env.getProperty("hibernate.connection.provider_class"));
+        config.addDataSourceProperty("cachePrepStmts", env.getProperty("hibernate.hikari.dataSource.cachePrepStmts"));
+        config.addDataSourceProperty("prepStmtCacheSize",
+                env.getProperty("hibernate.hikari.dataSource.prepStmtCacheSize"));
+        config.addDataSourceProperty("prepStmtCacheSqlLimit",
+                env.getProperty("hibernate.hikari.dataSource.prepStmtCacheSqlLimit"));
+        config.addDataSourceProperty("providerClass", env.getProperty("hibernate.connection.provider_class"));
 
-		return new HikariDataSource(config);
-	}
+        return new HikariDataSource(config);
+    }
 
-	private Properties hibernateProperties() {
-		final Properties properties = new Properties();
-		properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
-		properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-		properties.put("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
-		properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+    private Properties hibernateProperties() {
+        final Properties properties = new Properties();
+        properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
+        properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.hbm2ddl.import_files", env.getProperty("hibernate.hbm2ddl.import_files"));
+        return properties;
+    }
 
-		return properties;
-	}
-
-	@Bean
-	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
-		return transactionManager;
-	}
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+        return transactionManager;
+    }
 }
---
+++ src/main/resources/scripts/b2b-init.sql
@@ -0,0 +1,21 @@
+INSERT INTO app_config (id, config_key, config_value, created_by, created_date)
+VALUES (1, 'upload_path', '/Users/magicalcyber/Pictures/b2b_upload', 'B2B', curdate());
+
+INSERT INTO
+  brand (id, name, logo, created_by, created_date)
+VALUES
+  (70, 'STARBOARD SURF', NULL, 'B2B', curdate()),
+  (50, 'DRAKE', NULL, 'B2B', curdate()),
+  (40, 'SEVERNE', '/upload/brand/logo/severne.png', 'B2B', curdate()),
+  (30, 'AIRUSH', '/upload/brand/logo/airush.png', 'B2B', curdate()),
+  (29, 'STARBOARD SUP APPAREL', NULL, 'B2B', curdate()),
+  (10, 'STARBOARD', '/upload/brand/logo/starboard.png', 'B2B', curdate()),
+  (19, 'STARBOARD APPAREL', NULL, 'B2B', curdate()),
+  (20, 'STARBOARD SUP', NULL, 'B2B', curdate()),
+  (99, 'NON BRAND', NULL, 'B2B', curdate());
+
+
+INSERT INTO role (role_id) VALUES ('ROLE_ADMIN');
+INSERT INTO role (role_id) VALUES ('ROLE_USER');
+
+COMMIT;
