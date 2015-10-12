7c6c135d8d1fdd38795989bc0d145b90e02d9760
magicalcyber <magicalcyber@gmail.com>
2015-10-12 22:58:54 +0700

    new dto

---
+++ src/main/java/com/starboard/b2b/dto/ProductModelViewDTO.java
@@ -0,0 +1,14 @@
+package com.starboard.b2b.dto;
+
+import java.util.List;
+import java.util.Set;
+
+public class ProductModelViewDTO {
+	private List<ProductSearchResult> withnoseProduct;
+	private List<ProductSearchResult> noWithnoseProduct;
+	
+	private Set<String> withnoseProductSize;
+	private Set<String> noWithnoseProductSize;
+	
+	
+}
