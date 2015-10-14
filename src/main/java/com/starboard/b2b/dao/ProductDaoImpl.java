7fd4b6d344ee37f3d406e130c40bb8d055acefe9
magicalcyber <magicalcyber@gmail.com>
2015-10-14 23:03:06 +0700

    fix search condition

--- src/main/java/com/starboard/b2b/dao/ProductDaoImpl.java
+++ src/main/java/com/starboard/b2b/dao/ProductDaoImpl.java
@@ -89,7 +89,7 @@ public class ProductDaoImpl implements ProductDao {
 
 			if (StringUtils.isNotEmpty(condition.getKeyword())) {
 				sb.append("and (");
-				sb.append("   p.productModelId 	like :keyword ");
+				sb.append("   m.productModelName 	like :keyword ");
 				sb.append("or p.productNameEn 	like :keyword ");
 				sb.append("or p.productLength 	like :keyword ");
 				sb.append("or p.productBand 	like :keyword ");
