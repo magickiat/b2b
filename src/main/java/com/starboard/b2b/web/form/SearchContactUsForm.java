00b7a5804a3b6963f94f2db14845ef1e19438586
magicalcyber <magicalcyber@gmail.com>
2016-01-09 14:58:05 +0700

    search contact us

---
+++ src/main/java/com/starboard/b2b/web/form/SearchContactUsForm.java
@@ -0,0 +1,14 @@
+package com.starboard.b2b.web.form;
+
+public class SearchContactUsForm extends CommonSearchForm{
+	private String keyword;
+	
+	public String getKeyword() {
+		return keyword;
+	}
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	
+	
+}
