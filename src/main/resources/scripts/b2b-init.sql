04c517779a9f1c1bea8f20c2317f6daa1fe83d51
magicalcyber <magicalcyber@gmail.com>
2016-01-20 01:38:05 +0700

    baseline before UAT

--- src/main/resources/scripts/b2b-init.sql
+++ src/main/resources/scripts/b2b-init.sql
@@ -2,4 +2,7 @@
 INSERT INTO role (role_id) VALUES ('ROLE_ADMIN');
 INSERT INTO role (role_id) VALUES ('ROLE_USER');
 
+----- config -----
+insert into app_config(config_key, config_value) values('page.size', '12');
+
 COMMIT;
