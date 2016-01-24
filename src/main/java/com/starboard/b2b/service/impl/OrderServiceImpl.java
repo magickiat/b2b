75fc11ee9630ba73ade7016541776d66ae8cf5a0
magicalcyber <magicalcyber@gmail.com>
2016-01-25 06:16:11 +0700

    replace property file

--- src/main/java/com/starboard/b2b/service/impl/OrderServiceImpl.java
+++ src/main/java/com/starboard/b2b/service/impl/OrderServiceImpl.java
@@ -13,7 +13,6 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -60,9 +59,6 @@ import com.starboard.b2b.web.form.order.OrderSummaryForm;
 public class OrderServiceImpl implements OrderService {
 
 	private static final Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
-
-	@Value("${upload.path}")
-	private String uploadPath;
 
 	@Autowired
 	private ApplicationConfig applicationConfig;
--- src/main/java/com/starboard/b2b/util/ApplicationConfig.java
+++ src/main/java/com/starboard/b2b/util/ApplicationConfig.java
@@ -1,50 +1,54 @@
 package com.starboard.b2b.util;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource(value = "classpath:application-${spring.profiles.active}.properties")
 public class ApplicationConfig {
 
-	@Value("${page.size}")
-	private int pageSize;
+	//TODO with replace properties file
+	
+//	@Value("${page.size}")
+	private String pageSize;
 
-	@Value("${order.status.new}")
+//	@Value("${order.status.new}")
 	private String orderStatusNew;
 
-	@Value("${default.new.order.payment.term.id}")
+//	@Value("${default.new.order.payment.term.id}")
 	private String defaultPaymentTermId;
 
-	@Value("${default.product.currency}")
+//	@Value("${default.product.currency}")
 	private String defaultProductCurrency;
 
-	@Value("${default.product.unit}")
+//	@Value("${default.product.unit}")
 	private String defaultProductUnit;
 
-	@Value("${enabled.send.mail}")
-	private boolean enableSendMail;
+//	@Value("${enabled.send.mail}")
+	private String enableSendMail;
 
 	public int getPageSize() {
-		return pageSize;
+		return 12;
 	}
 
 	public boolean getEnabledSendMail() {
-		return enableSendMail;
+		return false;
 	}
 
 	public String getOrderStatusNew() {
-		return orderStatusNew;
+		return "10";
 	}
 
 	public String getNewOrderPaymentTermId() {
-		return defaultPaymentTermId;
+		return "3070AP";
 	}
 
 	public String getDefaultProductCurrency() {
-		return defaultProductCurrency;
+		return "TBA";
 	}
 
 	public String getDefaultProductUnit() {
-		return defaultProductUnit;
+		return "PCS";
 	}
 }
