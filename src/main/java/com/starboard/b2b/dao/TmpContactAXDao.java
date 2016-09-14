9dd49589c6d2d11db54b0a484630ce6ce8f5c708
Thanakit Jumparuang <jthanakit.dev@gmail.com>
2016-09-14 07:26:33 +0700

    -- Contact from AX

---
+++ src/main/java/com/starboard/b2b/dao/TmpContactAXDao.java
@@ -0,0 +1,15 @@
+package com.starboard.b2b.dao;
+
+import com.starboard.b2b.model.TmpContactFromAx;
+
+import java.util.List;
+
+public interface TmpContactAXDao {
+	List list();
+
+	TmpContactFromAx findById(Long contactId);
+	
+	void save(TmpContactFromAx obj);
+
+	void removeAll();
+}
--- src/main/java/com/starboard/b2b/service/impl/SyncB2BServiceImpl.java
+++ src/main/java/com/starboard/b2b/service/impl/SyncB2BServiceImpl.java
@@ -1,19 +1,26 @@
 package com.starboard.b2b.service.impl;
 
+import com.starboard.b2b.dao.ContactDao;
 import com.starboard.b2b.dao.OrderDao;
 import com.starboard.b2b.dao.OrderDetailDao;
 import com.starboard.b2b.dao.ProductDao;
 import com.starboard.b2b.dao.SoDao;
 import com.starboard.b2b.dao.SoDetailDao;
+import com.starboard.b2b.dao.TmpContactAXDao;
 import com.starboard.b2b.dao.TmpSoDao;
 import com.starboard.b2b.dao.TmpSoDetailDao;
+import com.starboard.b2b.model.Contact;
+import com.starboard.b2b.model.TmpContactFromAx;
 import com.starboard.b2b.service.SyncB2BService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service("syncB2BService")
 public class SyncB2BServiceImpl implements SyncB2BService {
@@ -27,20 +34,36 @@ public class SyncB2BServiceImpl implements SyncB2BService {
 	private TmpSoDao tmpSoDao;
     private TmpSoDetailDao tmpSoDetailDao;
 	private ProductDao productDao;
+	@Autowired
+	private TmpContactAXDao tmpContactAXDao;
+	@Autowired
+	private ContactDao contactDao;
 
 	@Override
+	@SuppressWarnings("unchecked")
 	@Transactional(rollbackFor = Exception.class)
 	public void syncContactFromAX() {
         log.info("Sync Contact from AX");
-        //Step 1: Get data from AX
-        //Step 2: Insert / Update into sync table
-        //Step 3: Remove table from AX
+		//Step 1: Get data from AX
+		List<TmpContactFromAx> tmpContactFromAXs = tmpContactAXDao.list();
+		if(tmpContactFromAXs != null && !tmpContactFromAXs.isEmpty()){
+			for(TmpContactFromAx tmpContactFromAx : tmpContactFromAXs){
+		//Step 2: Clear all match cust_id
+				contactDao.removeByCustId(tmpContactFromAx.getCustId());
+		//Step 3: Insert into sync table
+				Contact syncContact = new Contact();
+				BeanUtils.copyProperties(tmpContactFromAx, syncContact);
+				contactDao.save(syncContact);
+			}
+		//Step 4: Remove from AX
+			tmpContactAXDao.removeAll();
+		}
 	}
 
 	@Override
 	@Transactional(rollbackFor = Exception.class)
 	public void syncAddressFromAX() {
-        log.info("Sync Adddress from AX");
+        log.info("Sync Address from AX");
         //Step 1: Get data from AX
         //Step 2: Insert / Update into sync table
         //Step 3: Remove table from AX
